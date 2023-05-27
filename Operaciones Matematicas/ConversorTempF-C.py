3a2579559f0bc36b032e9cab872068e746ec07f9
Mazo667 <maxifava667@gmail.com>
2023-05-27 03:41:54 -0300

    agrego funciones de math

--- Operaciones Matematicas/ConversorTempF-C.py
+++ Operaciones Matematicas/ConversorTempF-C.py
@@ -1,6 +1,6 @@
 #Conversion de temperatura  de Celsius a Fahrenheit
 
-C = float(input("Ingrese la temperatura en C:\n"))
+C = float(input("Ingrese la temperatura en C:"))
 F = 1.8*C + 32;
 
 print("La temperatura en F es de %0.2f: "%F)
---
+++ Operaciones Matematicas/FuncionesMath.py
@@ -0,0 +1,16 @@
+import math
+
+abs = math.fabs(-3)       #Valor Absoluto
+expo = math.exp(1)     #Funcion Exponencial
+pote = math.pow(2,3)   #Potencia A elevado a B
+raiz2 = math.sqrt(2.0)  #Raiz Cuadrada
+cose = math.cos(0.7)   #Coseno de un angulo en radiantes
+seno = math.sin(0.707) #Seno de un angulo en radiantes
+tange = math.tan(1)     #Tangente de un angulo en radiantes
+grad = math.degrees(21) #Conversion de radiantes a grados
+pi = math.pi           #Obtencion de valor de pi
+e = math.e          #Obtencion de valor de e
+arcose = math.acos       #Arco seno de un valor (angulo)
+t = math.asin       #Arco seno de un valor (angulo)
+
+print(expo)
