0c34bd776dc9d5f06656065ab7566c9436b37f72
Mazo667 <68477400+Mazo667@users.noreply.github.com>
2023-06-01 01:02:17 -0300

    Agrego Fibonacci recursivo y iterativo

---
+++ Operaciones Matematicas/fibonacci.py
@@ -0,0 +1,25 @@
+def fibonacciR(n):
+    """Calculo Fibonacci Recursivo"""
+    if(n == 0 or n == 1):
+        return n
+    else:
+        return fibonacciR(n-1) + fibonacciR(n-2)
+    
+def fibonacciI(n):
+    """Calculo Fibonacci Iterativo"""
+    n0=0
+    n1=1
+    fib=0
+    if(n == 0 or n == 1):
+        fib = n
+    else:
+        i = 2
+        while (i <= n):
+            fib = n0 + n1
+            n0 = n1
+            n1 = fib
+            i += 1
+    return fib
+
+print(fibonacciR(6))
+print(fibonacciI(6))
