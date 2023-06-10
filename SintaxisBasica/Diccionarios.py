e315b1539eece3b5186b3f7e7fb88ced7e9cbb1d
Mazo667 <maxifava667@gmail.com>
2023-06-10 20:42:31 -0300

    Diccionarios y sus funciones

--- SintaxisBasica/Diccionarios.py
+++ SintaxisBasica/Diccionarios.py
@@ -1,3 +1,4 @@
+"""Coleccion Desordenada de pares clave-valor. Las claves son inmutables, en cambio los valores si"""
 Receta_Guiso = {"arroz":0.5,"carne":1,"cebolla":2}
 
 print(Receta_Guiso.get("arroz"))
