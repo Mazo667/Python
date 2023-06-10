0c2a1c54949a101550d6a5403b10b19a3a4323c6
Mazo667 <maxifava667@gmail.com>
2023-06-10 20:35:37 -0300

    Tuplas y sus funciones basicas

---
+++ SintaxisBasica/Tuplas.py
@@ -0,0 +1,6 @@
+a = (1,2,'python')
+
+print(type(a))
+print(a[2]) #Nos imprime el valor que esta en el indice 2
+print(a.index('python'))   #Nos dice en que indice esta el valor
+print(a.count(2))   #Retorna el numero de veces que aparece un valor especificado
